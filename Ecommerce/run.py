a98660edea4548fb4b4871ceadb91b85304e9026
favour <favourchukwuemeka004@gmail.com>
2024-08-06 01:34:34 +0100

    Performed some changes

--- Ecommerce/run.py
+++
@@ -1,9 +0,0 @@
-#!C:\Users\ka_ndi\AppData\Roaming\Python\Python310\Scripts\venv\Scripts\python.exe
-print("Content-Type: text/html\n")
-
-from app import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
